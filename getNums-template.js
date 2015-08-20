206564b0311e8debf309ece347604754623dac1a
Jonathan Davis <jdstep23@gmail.com>
2015-08-19 22:31:10 -0400

    Update and rename iterate-template.js to getNums-template.js

---
+++ getNums-template.js
@@ -0,0 +1,7 @@
+// returns the nums at a given index for the passed in array
+// used for hacker rank solutions to easily retrieve the numbers
+function getNums(array, index) {
+  index = index || 0;
+  var result = array.split('\n')[index].split(' ').map(function(numberStr) { return parseInt(numberStr);});
+  return result;
+}
